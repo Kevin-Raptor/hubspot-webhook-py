0a0b56bf977075d92300048e968cb7f66a842c0a
Kevin-Raptor <62995016+Kevin-Raptor@users.noreply.github.com>
2024-08-12 17:08:50 +0530

    Refactor hit_webhook function to accept separate parameters

--- webhookTest.py
+++ webhookTest.py
@@ -13,8 +13,7 @@ def call_webhook(url, payload):
 @flow(log_prints=True)
 def hit_webhook(param1, param2):
     url = "https://webhook.site/ed1c1eb5-6e05-4358-a8b6-a8f3cfdd030e"
-    payload = {"message1": param1 }
-    payload = {"message2": param2 }
+    payload = {"message1": param1,"message2": param2 }
     resp = call_webhook(url, payload)
     print('~~~~~~~~response is ' + resp)
 
